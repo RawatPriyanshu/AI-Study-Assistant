220f96825714b5e7d8b1425f31b8dab33355cdd3
Vivek <vivekkr48027@gmail.com>
2026-08-20 12:35:22 +0530

    hero section banner completed

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -6,7 +6,7 @@ const LandingPage = () => {
     <div>
       <section className="hero-section">
         <div className="container">
-          <div className="hero-wrap w-full flex flex-row items-center justify-center">
+          <div className="hero-wrap w-full flex flex-row items-center">
             <div className="hero-content max-w-112.5 w-full flex flex-col gap-6.5">
               <p className="!text-[var(--bg-tertiary-color)] !font-extrabold uppercase">
                 Smarter Studying. Powered by AI
@@ -34,81 +34,89 @@ const LandingPage = () => {
                 </button>
               </div>
             </div>
-            <div className="hero-banner">
-              <div className="hero-banner-cards flex flex-col max-w-75 shadow-2xl py-4">
+            <div className="hero-banner relative">
+              <div className="hero-banner-cards rounded-2xl flex flex-col max-w-75 w-full shadow-2xl py-4 bg-white">
                 <div className="banner-heading flex flex-row justify-between items-center w-full pb-3.5 pl-6 pr-14 pr- border-b border-[#dddddd]">
-                  <p>My Notes</p>
+                  <p className="text-m">My Notes</p>
                   <span className="material-symbols-outlined cursor-pointer">
                     trending_flat
                   </span>
                 </div>
                 <div className="hero-notes-content flex flex-col gap-6">
-                  <div className="para flex flex-col gap-4 pt-2.5  pl-6 pr-14">
-                    <h2 className="text-[20px] font-bold">
-                      Javascript Closures
-                    </h2>
-                    <p>
+                  <div className="para flex flex-col gap-4 pt-2.5 pl-6 pr-12">
+                    <h2 className="text-xl font-bold">Javascript Closures</h2>
+                    <p className="text-sm">
                       A closure is combinaition of a function budled
                       together(enclosed) with reference to its surrounding state
                       (the laxical envtioment).
                     </p>
                   </div>
-                  <div className="para pl-6 pr-14">
-                    <h3 className="text-[15px] font-bold">Key Points</h3>
-                    <ul className="list-disc pl-[20px] pt-[15px]">
+                  <div className="para pl-6 pr-12">
+                    <h3 className="text-xl font-bold">Key Points</h3>
+                    <ul className="list-disc pl-3 pt-3.5">
                       <li>
-                        <p>
+                        <p className="text-sm">
                           A closure gives a function access to its outer scope.
                         </p>
                       </li>
                       <li>
-                        <p>Closures are commonly used in cllbacks.</p>
+                        <p className="text-sm">
+                          Closures are commonly used in cllbacks.
+                        </p>
                       </li>
                       <li>
-                        <p>Variables can remain accessable after execution.</p>
+                        <p className="text-sm">
+                          Variables can remain accessable after execution.
+                        </p>
                       </li>
                     </ul>
                   </div>
-                  <div className="hero-banner-card-tags flex flex-row justify-between flex-wrap pl-6 pr-14 ">
-                    <span className="bg-[#dbdbdb91] px-5 py-1 rounded-4xl">
-                      Javascript
+                  <div className="hero-banner-card-tags flex flex-row gap-2 flex-wrap pl-6 pr-14 ">
+                    <span className="bg-[#dbdbdb91] px-5 py-1 rounded-4xl text-sm">
+                      <p>Javascript</p>
                     </span>
-                    <span className="bg-[#dbdbdb91] px-5 py-1 rounded-4xl">
-                      Funcitons
+                    <span className="bg-[#dbdbdb91] px-5 py-1 rounded-4xl text-sm">
+                      <p>Funcitons</p>
                     </span>
                   </div>
                 </div>
               </div>
-              <div className="hero-banner-cards">
-                <div className="banner-heading">
-                  <p>Ai Summary</p>
+              <div className="hero-banner-cards absolute top-10 left-70 rounded-2xl flex flex-col max-w-75 w-full shadow-2xl pt-5 pb-5 bg-white p-6">
+                <div className="banner-heading flex flex-row justify-between items-center pb-5">
+                  <h2 className="text-[20px] font-bold">Ai Summary</h2>
                   <span className="material-symbols-outlined">bookmark</span>
                 </div>
-                <div className="hero-notes-content">
+                <div className="hero-notes-content flex flex-col gap-6">
                   <div className="para">
-                    <p>
-                      Closures allows a function to remember and access
-                      variables from its laxical enviroment even the outer
-                      function has finished executing.
+                    <p className="text-sm">
+                      Closures allows a function to remember <br /> and access
+                      variables from its laxical <br /> enviroment even the
+                      outer function <br /> has finished executing.
                     </p>
                   </div>
-                  <div className="hero-banner-card-tags">
-                    <h3>Key Points</h3>
-                    <div className="hero-banner-card-tags">
-                      <span>Javascript</span>
-                      <span>Funcitons</span>
+                  <div className="hero-banner-card-tags flex flex-col gap-3">
+                    <h3 className="text-[15px] font-bold">Important Terms</h3>
+                    <div className="hero-banner-card-tags flex flex-row flex-wrap gap-2">
+                      <span className="bg-[#dbdbdb91] px-5 py-1 rounded-4xl text-sm">
+                        Closure
+                      </span>
+                      <span className="bg-[#dbdbdb91] px-5 py-1 rounded-4xl text-sm">
+                        Lexical Scope
+                      </span>
+                      <span className="bg-[#dbdbdb91] px-5 py-1 rounded-4xl text-sm">
+                        Callback
+                      </span>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="hero-banner-cards">
-                <div className="hero-notes-content">
-                  <div className="para">
-                    <h3>Key Points</h3>
-                    <ul>
-                      <li>What does a closure preserve ?</li>
-                    </ul>
-                  </div>
+              <div className="hero-banner-cards absolute bottom-5 left-80 rotate-355 rounded-2xl border-b-5 border-b-[var(--bg-tertiary-color)] flex flex-col max-w-75 w-full shadow-[0_-12px_35px_rgba(0,0,0,0.12),0_12px_30px_rgba(0,0,0,0.10)] pt-5 pb-7 bg-white p-6">
+                <div className="para">
+                  <ul>
+                    <li className="text-2xl font-bricol-bold">
+                      What does a closure preserve ?
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
